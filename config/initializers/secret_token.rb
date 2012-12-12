61ee43b730b0d40812f2b92e2a9f87aebc192658
Hisham Malik <hisham.malik@gmail.com>
2012-12-12 15:00:50 +0500

    Initial commit with rspec installed

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PtSummaryReports::Application.config.secret_token = 'fec2da91d85e9be426984f5d6f13d8bdd137246ac426f6e3c4d4c7aed249eeb575152a0976ed43141b39d576f5a622955c3af2884c3af04f6d4e73964bb33677'
