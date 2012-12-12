0d920290eefbd0d88a8185f7329ff9eaf9853770
Hisham Malik <hisham.malik@gmail.com>
2012-12-12 16:41:39 +0500

    configured devise

--- config/application.rb
+++ config/application.rb
@@ -56,6 +56,10 @@ module PtSummaryReports
     # Enable the asset pipeline
     config.assets.enabled = true
 
+    # Force application to not access the DB or load models when
+    # precompiling assets
+    config.assets.initialize_on_precompile = false
+    
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
